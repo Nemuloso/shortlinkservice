3dd93224f506221a1b694460f8e5972dd2a46dd4
Nemuloso <hermes.ste@gmail.com>
2019-06-26 08:35:45 +0200

    wip

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static( __dirname + "/styles" ));
+const monk = require("monk");
+const db = monk("localhost:27017/shortlinkdb");
 
-app.get('/test', function (req, res) {
-    res.sendFile( __dirname + "/test.html" );
- })
+app.use(express.static(__dirname + "/styles"));
 
-var server = app.listen(3000, function () {
-    var host = server.address().address
-    var port = server.address().port
-    
-    console.log("Example app listening at http://%s:%s", host, port)
- })
+app.get("/test", function (req, res) {
+    res.sendFile(__dirname + "/test.html");
+})
+
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
+
+app.get("/shorturl", function (req, res) {
+    const db = req.db;
+
+    const shortURL = generateShortUrl();
+
+    const collection = db.get("urls");
+
+    collection.insert({
+        "shorturl": shortURL,
+        "longurl": userEmail
+    }, function (err, doc) {
+        if (err) {
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            res.send({
+                "shorturl": shortURL
+            });
+        }
+    });
+});
+
+app.post("/longurl", function (req, res) {
+    const db = req.db;
+
+    const shortURL = req.body.data;
+
+    const collection = db.get("urls");
+
+    collection.find({ shorturl: shortURL }, {}, function (e, entry) {
+        res.send({
+            "longurl": entry
+        });
+    });
+});
+
+const server = app.listen(3000, function () {
+    const host = server.address().address
+    const port = server.address().port
+
+    console.log("Listening at http://%s:%s", host, port)
+})
